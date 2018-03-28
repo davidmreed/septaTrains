045ffaa1eadd9e2065e939f8b4d7f07157a4baec
David Reed <david.reed@radian.biz>
2018-03-28 16:13:48 -0400

    Fix entitySelector issues

--- force-app/main/default/aura/septaEntitySelector/septaEntitySelectorController.js
+++ force-app/main/default/aura/septaEntitySelector/septaEntitySelectorController.js
@@ -12,10 +12,8 @@
     },
 
     handleComboNavigate : function(component, event, helper) {
-        console.log('component = ' + component + ' event = ' + event + ' helper = ' + helper);
-        console.log('event params = ' + event.getParams().arguments);
         var evt = component.getEvent('navigateEvent');
-        var entity = event.getParam('value');
+        var entity = component.get('v.selectedEntity');
         
         evt.setParam('entity', { entity: entity, type: (Number.isNaN(Number(entity)) ? 'STATION' : 'TRAIN') });                    
         evt.fire();
